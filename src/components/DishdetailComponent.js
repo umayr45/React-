a587eeb930d87f9ec5170a548986cd4b96576bca
Umayr Farooqui <umayrfarooqui45@gmail.com>
2020-07-24 20:09:52 +0530

    Container concept done

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+
+class Dishdetail extends Component {
+
+
+    renderComments(comments){
+        if (comments===null){
+            return (<div></div>)
+        }   
+        const cmnts=comments.map(comment=>{
+            return (
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>--{comment.author},  {new Intl.DateTimeFormat('en-US',{month:'short',day:'2-digit',year:'numeric'}).format(new Date(comment.date))}
+                    </p>
+                   
+                </li>
+            )
+        })
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul type="none">
+                        
+                    {cmnts}
+                </ul>    
+
+
+            </div>
+        )
+            
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <div className='col-12 col-md-5 m-1'>
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                    
+                </div>
+            )
+        }
+        else {
+            return (<div></div>)
+        }
+    }
+ 
+    render() {
+        const dish = this.props.dish
+        if (dish == null) {
+            return (<div></div>)
+        }
+        const dishItem = this.renderDish(dish)
+        const commentItem = this.renderComments(dish.comments)
+        return (
+            <div className='container'>
+                <div className="row">
+                    
+                    {dishItem}
+                    {commentItem}
+                        
+
+                </div>
+                    
+                
+            </div>
+        )
+    }
+}
+
+export default Dishdetail
